8920bde56507c643ca897745f8d9cf1697a87dac
jrivas201918 <joshuarivas19990820@outlook.com>
2025-06-26 15:52:10 +0800

    Significant changes for login adding the registration of account.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,11 @@
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-
+            @if (Route::has('register'))
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 ms-4" href="{{ route('register') }}">
+                    {{ __('Register') }}
+                </a>
+            @endif
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
